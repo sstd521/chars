24e5d3a0a25dfab1d4181bc0a82dcdbba8447bf0
sstd521 <sstd521@gmail.com>
2022-09-19 21:19:17 +0800

    1

---
+++ src/settings.cd702.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/未命名.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"01588",main:"27462"}};
